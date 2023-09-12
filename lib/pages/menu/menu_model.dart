0c4ec98fa8052fdb23c002529ad1163b57bcd286
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-12 18:45:45 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/menu/menu_model.dart
+++ lib/pages/menu/menu_model.dart
@@ -2,7 +2,6 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
@@ -10,12 +9,6 @@ class MenuModel extends FlutterFlowModel {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
-  // State field(s) for RatingBar widget.
-  double? ratingBarValue1;
-  // State field(s) for RatingBar widget.
-  double? ratingBarValue2;
-  // State field(s) for RatingBar widget.
-  double? ratingBarValue3;
 
   /// Initialization and disposal methods.
 
